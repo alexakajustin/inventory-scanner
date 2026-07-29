db3da067fc1a3b280314a906cb28d26fc201de71
alexakajustin <alexakajustin@gmail.com>
2026-07-29 15:29:46 +0300

    added gitignore and removed bad files

--- Inventory-Scanner-Windows/start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Snipe-IT Barcode Scanner Server
-echo ===================================================
-echo Pornire Server (Nu inchide aceasta fereastra)
-echo ===================================================
-cd /d "%~dp0"
-node.exe server.js
-pause
